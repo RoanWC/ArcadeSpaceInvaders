decae0a42dda2aa2a08b94b47fe8034edc06d4bd
RoanWC <Roanwc@gmail.com>
2017-03-18 17:17:00 -0400

    started making interfaces and base classes

---
+++ ArcadeSpaceInvaders/ArcadeSpaceInvadersLibrary/IMovable.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcadeSpaceInvadersLibrary
+{
+    interface IMovable
+    {
+        Vector2 Position
+        {
+            get;
+            set;
+        }
+        int Speed
+        {
+            get;
+            set;
+        }
+
+        void move();
+
+    }
+}
---
+++ ArcadeSpaceInvaders/ArcadeSpaceInvadersLibrary/Projectile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace ArcadeSpaceInvadersLibrary
+{
+    /// <summary>
+    /// defines the base behavior of projectiles such as bullets and torpedos
+    /// </summary>
+    public abstract class Projectile
+    {
+        private Vector2 position;
+        private int speed;
+
+        public Vector2 Position
+        {
+            get { return position; }
+            set { this.position = value; }
+        }
+
+        public abstract void move();
+         
+    }
+}
