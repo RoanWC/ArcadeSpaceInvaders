c4a714e49b3da942f6b0d9f616514f6f74f9a555
RoanWC <Roanwc@gmail.com>
2017-03-19 12:43:27 -0400

    started the ship abstract class

---
+++ ArcadeSpaceInvaders/ArcadeSpaceInvadersLibrary/EnemyShip.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace ArcadeSpaceInvadersLibrary
+{
+    class EnemyShip : IMovable, IShootable, IShooter
+    {
+        //private fields
+        private Vector2 speed;
+        private int health;
+        private int points;
+        private Rectangle boundingBox;
+        private int screeenWidth;
+        private int screenHeight;
+
+
+        public Rectangle BoundingBox
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Health
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Points
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Vector2 Speed
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Destroy()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GotShot()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoveDown()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoveLeft()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoveRight()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoveUp()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Shoot()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ArcadeSpaceInvaders/ArcadeSpaceInvadersLibrary/Ship.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcadeSpaceInvadersLibrary
+{
+    class Ship:IMovable, IShootable, IShooter
+    {
+        private Vector2 speed;
+        private int points;
+        private Rectangle boundingBox;
+        private int screeenWidth;
+        private int screenHeight;
+
+        public Rectangle BoundingBox
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Vector2 Speed
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Health
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Points
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void MoveUp()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoveDown()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoveLeft()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoveRight()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GotShot()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Destroy()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Shoot()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
